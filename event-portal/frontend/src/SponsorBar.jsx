f9fd599752f607bc1a6ba99a051d797e289716a5
WeezYMeyer <136649870+WeezYMeyer@users.noreply.github.com>
2026-09-07 10:45:51 +0200

    Create SponsorBar.jsx

---
+++ event-portal/frontend/src/SponsorBar.jsx
@@ -0,0 +1,19 @@
+const SPONSOR_COUNT = 7;
+
+export default function SponsorBar() {
+  return (
+    <div className="sponsor-bar">
+      <div className="sponsor-bar-label">Proudly Supported By</div>
+      <div className="sponsor-bar-logos">
+        {Array.from({ length: SPONSOR_COUNT }, (_, i) => i + 1).map((n) => (
+          <img
+            key={n}
+            src={`/sponsors/sponsor-${n}.png`}
+            alt={`Sponsor ${n}`}
+            className="sponsor-logo"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
